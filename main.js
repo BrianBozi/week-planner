34e64a59ad858f6d3ea3e133dd00d77789c54e26
Brian Bozigian <bbozigian@outlook.com>
2021-05-28 15:28:33 -0700

    added some logic

--- main.js
+++ main.js
@@ -1,8 +1,41 @@
+var data = {
+  // view: 'entry-form',
+  entries: [],
+  editing: null,
+  nextEntryId: 1
+};
+
+var todoJSON = localStorage.getItem('javascript-local-storage');
+
+if (todoJSON !== null) {
+  data = JSON.parse(todoJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var oldTodoJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', oldTodoJSON);
+});
+
+// real logic
 var $addEntryButton = document.querySelector('.addEntry');
 var $modalContainerOff = document.querySelector('.modalContainerOff');
 
 $addEntryButton.addEventListener('click', function (event) {
   $modalContainerOff.className = 'modalContainerOn';
+});
+
+var $form = document.querySelector('#form');
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  var post = {
+    day: $form.days.value,
+    time: $form.time.value,
+    notes: $form.notes.value
+    // entryId: data.nextEntryId
+  };
+
+  return post;
 });
 
 // function render(table) {
