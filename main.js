02e842aadd80e5020cca5a7c3ad5ed95724a6501
Chris Kim <midaskim6969@yahoo.com>
2021-05-28 15:00:10 -0700

    week-planner.

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ var $modalContainerOff = document.querySelector('.modalContainerOff');
 $addEntryButton.addEventListener('click', function (event) {
   $modalContainerOff.className = 'modalContainerOn';
 });
+
+// function render(table) {
+//   var tbody = document.createElement('tbody');
+//   var onetr = document.createElement('tr');
+//   var td = document.createElement('td');
+//   var tdTwo = document.createElement('td');
+//   var button = document.createElement('button');
+// }
